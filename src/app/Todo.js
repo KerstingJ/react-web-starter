4416c98bbf54d38c4ac7893d22ec97a0f0a70511
Josh <Kersting.Josh@Gmail.com>
2019-09-22 20:29:37 -0400

    simple todo app completed

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: ""
+    };
+  }
+
+  submitTodo(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({ todos, newTodo: "" });
+  }
+
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <form>
+          <input
+            type="text"
+            name="newTodo"
+            value={this.state.newTodo}
+            onChange={this.submitTodo.bind(this)}
+          />
+          <button onClick={this.handleClick.bind(this)}>create</button>
+        </form>
+        <ul>
+          {this.state.todos.map((todo, i) => (
+            <li key={i}>{todo}</li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
